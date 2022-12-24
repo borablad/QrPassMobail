b26ff2648c82bd9dbc0d5a961c815401c09ea771
Bora_blad <96587810+borablad@users.noreply.github.com>
2022-12-24 16:02:30 +0600

    Добавьте файлы проекта.

---
+++ QrPassMobail/QrPassMobail/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using QrPassMobail.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace QrPassMobail.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ QrPassMobail/QrPassMobail/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using QrPassMobail.Models;
+using QrPassMobail.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace QrPassMobail.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
