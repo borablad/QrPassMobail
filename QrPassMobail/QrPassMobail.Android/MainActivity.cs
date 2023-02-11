04063f62b4a8bced9974848dfff3187e934114f4
javaBoy2500Genius <110668707+javaBoy2500Genius@users.noreply.github.com>
2023-02-11 14:37:13 +0600

    check nfc tipa reader

--- QrPassMobail/QrPassMobail.Android/MainActivity.cs
+++ QrPassMobail/QrPassMobail.Android/MainActivity.cs
@@ -4,16 +4,22 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Plugin.NFC;
+using Android.Content;
+using Android.Nfc;
+using QrPassMobail.Views;
 
 namespace QrPassMobail.Droid
 {
     [Activity(Label = "QrPassMobail", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [IntentFilter(new[] { NfcAdapter.ActionNdefDiscovered }, Categories = new[] { Intent.CategoryDefault }, DataMimeType = MainPage.MIME_TYPE)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
+           
             base.OnCreate(savedInstanceState);
-
+            CrossNFC.Init(this);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
@@ -23,6 +29,20 @@ namespace QrPassMobail.Droid
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+            // Plugin NFC: Restart NFC listening on resume (needed for Android 10+) 
+            CrossNFC.OnResume();
+        }
+        protected override void OnNewIntent(Intent intent)
+        {
+            base.OnNewIntent(intent);
+
+            // Plugin NFC: Tag Discovery Interception
+            CrossNFC.OnNewIntent(intent);
         }
     }
 }
