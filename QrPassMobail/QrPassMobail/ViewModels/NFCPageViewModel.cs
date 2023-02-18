49e5e0cfe06f4ee9909de458c21b9c8efe978179
Bora_blad <96587810+borablad@users.noreply.github.com>
2023-02-18 13:37:22 +0600

    end commit maby

--- QrPassMobail/QrPassMobail/ViewModels/NFCPageViewModel.cs
+++ QrPassMobail/QrPassMobail/ViewModels/NFCPageViewModel.cs
@@ -1,11 +1,24 @@
 ﻿using System;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm;
+using CommunityToolkit.Mvvm.ComponentModel;
 namespace QrPassMobail.ViewModels
 {
 	public partial class NFCPageViewModel:BaseViewModel
 	{
+
+		[ObservableProperty]
+		private bool nFCScan = true;
+
 		public NFCPageViewModel()
 		{
 		}
+
+		[RelayCommand]
+		public async void BackToQR()
+		{
+			await AppShell.Current.Navigation.PopAsync();
+		} 
 	}
 }
 
